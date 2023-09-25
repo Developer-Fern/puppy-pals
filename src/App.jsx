a1587ea2186ca180f66e38f74093e04e9885b673
Fern <FernandoFlores.private@gmail.com>
2023-09-25 02:00:47 -0400

    added puppy data mapped over and rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  function handleClick(id) {
+    setFeatPupId(id)
+  }
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
       <div>
+        {puppies.map(puppy => (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        ))}
+
+        {featPupId && (
+          <div className="selected">
+            <h2>{featuredPup.name}</h2>
+            <ul className="list">
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   )
 }
